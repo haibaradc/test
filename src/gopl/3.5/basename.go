9bcf018251df4dccb4370df4909dd82306813867
zhangbo <zhangbo@zhinengguo.com>
2017-09-13 10:58:04 +0800

    3.11

--- src/gopl/3.5/basename.go
+++ src/gopl/3.5/basename.go
@@ -38,25 +38,39 @@ func basename2(s string) string {
 // 12,345
 func comma(s string) string {
 	var s1 = ""
-	l := len(s)
+	var s2 = ""
+	var s3 = ""
+	index := strings.Index(s, ".")
+	if index != -1 {
+		s2 = s[index:]
+		s3 = s[:index]
+	} else {
+		s3 = s
+	}
+	if s3[0] == '-' || s3[0] == '+' {
+		s1 += s3[0:1]
+		s3 = s3[1:]
+	}
+	l := len(s3)
 	if l <= 3 {
-		s1 = s
+		s1 += s3
 		return s1
 	}
 	l2 := l % 3
 	if l2 == 0 {
 		l2 = 3
 	}
-	s1 += s[0:l2]
+	s1 += s3[0:l2]
 	for i := l2; i < l; i += 3 {
 		s1 += ","
-		s1 += s[i : i+3]
+		s1 += s3[i : i+3]
 	}
+	s1 += s2
 	return s1
 }
 
 func main() {
 	fmt.Println(basename("/a/b/c.d.go"))
 	fmt.Println(basename2("/a/b/c.d.go"))
-	fmt.Println(comma("123464542332532432312312542543523666666"))
+	fmt.Println(comma("+123464542332532432312312542543523.666666"))
 }
